69634c1320846c600fe01f2eefc879d28024648f
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-11 10:35:22 +0800

    first page

--- base/views.py
+++ base/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import JsonResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Hello, this is my first Django page!')
+def first_page(request):
+    return render(request, 'base/first_page.html')
 
 def greeting(request):
     return JsonResponse({'message': 'Hello, welcome to our API!'})
--- super_note_django/urls.py
+++ super_note_django/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from base.views import home
+from django.urls import include, path
 from base.views import greeting
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('base.urls')),
     path('greeting/', greeting, name='greeting'),
 ]
