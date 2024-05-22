06aa2f84a72547ab49ae0b97c6ad75d213277ec2
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-22 23:15:01 +0800

    test

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import first_page
+from base.views import greeting
+
 
 urlpatterns = [
     path('', first_page, name='first_page'),
+    path('greeting/', greeting, name='greeting'),
 ]
--- super_note_django/urls.py
+++ super_note_django/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from base.views import greeting
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('django/', include('base.urls')),
-    path('greeting/', greeting, name='greeting'),
+    
 ]
