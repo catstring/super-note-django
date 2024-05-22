8b4db52db81c1bef61cbdcda869c7eee287ec420
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-22 16:33:51 +0800

    empty database

--- super_note_django/settings.py
+++ super_note_django/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'super_note_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
